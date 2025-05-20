cf104d66cc9c4aa3af720f52306fcb6a12f0f23d
Enes YAPICI <enesyapici.dev@gmail.com>
2025-05-20 21:15:56 +0300

    feat(routing): add basic routing with Home, Bookshelf, and Movieshelf pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,22 @@
 import { useState } from "react";
 import "./App.css";
-import Moviecard from "./components/Moviecard/Moviecard";
-import Movielist from "./components/Movielist/Movielist";
 import Bookshelf from "./pages/Bookshelf/Bookshelf";
 import Movieshelf from "./pages/Movieshelf/Movieshelf";
+import Home from "./pages/Home/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Movieshelf />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/bookshelf" element={<Bookshelf />}></Route>
+          <Route path="/movieshelf" element={<Movieshelf />}></Route>
+        </Routes>
+      </main>
     </>
   );
 }
