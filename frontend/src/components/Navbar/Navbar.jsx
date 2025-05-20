0152ffd92cab4d687703d9bf4e49652dce5ad60c
Enes YAPICI <enesyapici.dev@gmail.com>
2025-05-20 22:26:51 +0300

    feat(navbar): add basic styling to navbar component

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -5,13 +5,23 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-brand">
-        <Link to="/">Mindshelfs</Link>
-      </div>
-      <div className="navbar-links">
-        <Link to="/">Home</Link>
-        <Link to="/movieshelfs">Movieshelfs</Link>
-        <Link to="/bookshelfs">Bookhelfs</Link>
+      <div className="navbar-content">
+        <div className="navbar-brand">
+          <Link to="/" className="nav-brand">
+            Mindshelfs
+          </Link>
+        </div>
+        <div className="navbar-links">
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+          <Link to="/movieshelf" className="nav-link">
+            Movieshelfs
+          </Link>
+          <Link to="/bookshelfs" className="nav-link">
+            Bookhelfs
+          </Link>
+        </div>
       </div>
     </nav>
   );
