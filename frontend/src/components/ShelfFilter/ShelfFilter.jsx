41588227b1a81f538ad8a94e2fa17f2077e62b6f
Enes YAPICI <enesyapici.dev@gmail.com>
2025-05-22 16:46:41 +0300

    style(general):stilize navbar, searchbar and categorybar

--- frontend/src/components/ShelfFilter/ShelfFilter.jsx
+++ frontend/src/components/ShelfFilter/ShelfFilter.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./ShelfFilter.css";
 
-const ShelfFilter = ({ categories, onCategoryChange }) => {
+const ShelfFilter = ({ categories, onCategoryChange, selected }) => {
   return (
     <div className="category-bar">
       {categories.map((cat) => (
         <button
           key={cat.title}
-          className="category-button"
+          className={`category-button${
+            selected === cat.title ? " active" : ""
+          }`}
           onClick={() => onCategoryChange(cat.title)}
         >
           {cat.title}
