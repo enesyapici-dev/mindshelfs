d406f277c0523aa12998a665c18179057f021d05
Enes YAPICI <enesyapici.dev@gmail.com>
2025-05-23 11:04:56 +0300

    feat(backend):create node project

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import "./Sidebar.css";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <div className="sidebar-cont">
       <div className="menu-content">
         <ul className="sidemenu-list">
-          <li className="sidemenu-li">Home</li>
-          <li className="sidemenu-li">Movies</li>
-          <li className="sidemenu-li">Books</li>
+          <li className="sidemenu-li">
+            <Link to="/" className="link">
+              Home
+            </Link>
+          </li>
+          <li className="sidemenu-li">
+            <Link to="/movieshelf" className="link">
+              Movieshelfs
+            </Link>
+          </li>
+          <li className="sidemenu-li">
+            <Link to="/bookshelfs" className="link">
+              Bookhelfs
+            </Link>
+          </li>
         </ul>
       </div>
       <div className="menu-button">
