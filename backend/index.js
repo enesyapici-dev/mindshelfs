5169e15f078b97fc126047d6f14785e85bcafd0c
Enes YAPICI <enesyapici.dev@gmail.com>
2025-05-23 18:24:09 +0300

    feat(backend): implement cors

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
 import { PORT, mongoDBURL } from "./config.js";
-
 import moviesRoute from "./routes/moviesRoute.js";
-
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
-
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("Welcome to mindshelfs");
