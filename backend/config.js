a19097d249b8effc0e6b505e6ee399a91c44cdf1
Enes YAPICI <enesyapici.dev@gmail.com>
2025-05-23 11:44:50 +0300

    feature(backend):connet app to mongodb

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+export const mongoDBURL =
+  "mongodb+srv://root:stroot@mindshelves.bx4lsr4.mongodb.net/?retryWrites=true&w=majority&appName=Mindshelves";
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { PORT } from "./config.js";
+import mongoose from "mongoose";
+import { PORT, mongoDBURL } from "./config.js";
 
 const app = express();
 
@@ -8,6 +9,14 @@ app.get("/", (request, response) => {
   return response.status(234).send("Welcome to mindshelfs");
 });
 
-app.listen(PORT, () => {
-  console.log(`app listening on port:${PORT}`);
-});
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("app connected to db");
+    app.listen(PORT, () => {
+      console.log(`app listening on port:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
