9fbabf5b893dd5d2ca9bd12443387387f1b5da0f
Enes YAPICI <enesyapici.dev@gmail.com>
2025-06-04 14:21:25 +0300

    feat(backend): create getPopulerBooks function

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 const API_KEY = "7b6a9e51b8f943d3196812134999bf54";
 const BASE_URL = "https://api.themoviedb.org/3";
 
+//TMDB
 export const getPopulerMovies = async () => {
   const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
   const data = await response.json();
@@ -22,4 +23,19 @@ export const getMovieDetails = async (movieId) => {
   );
   const data = await response.json();
   return data;
+};
+
+//Open-Library API
+const BASE_URL_OL = "https://openlibrary.org";
+export const getPopularBooksByEditions = async () => {
+  try {
+    const response = await fetch(
+      `${BASE_URL_OL}/search.json?sort=editions&limit=20`
+    );
+    const data = await response.json();
+    return data.docs;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 };
