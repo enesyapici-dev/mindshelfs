eb1c43e90b88a1435a7c40c788b87f54edb42d97
Enes YAPICI <enesyapici.dev@gmail.com>
2025-06-04 14:29:39 +0300

    feat(bookshelf): create bookshelf component structure

--- frontend/src/pages/Bookshelf/Bookshelf.jsx
+++ frontend/src/pages/Bookshelf/Bookshelf.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Allbooks from "../../components/Allbooks/Allbooks";
+import ShelfFilter from "../../components/ShelfFilter/ShelfFilter";
+import Searchbar from "../../components/Searchbar/Searchbar";
+import "./Bookshelf.css";
+
+const categories = [
+  { title: "All Books" },
+  { title: "Read" },
+  { title: "Read Later" },
+];
 
 const Bookshelf = () => {
-  return <div></div>;
+  return (
+    <div className="books-page">
+      <div className="bookshelf-cont">
+        <Searchbar placeholder={"Search books..."} />
+        <ShelfFilter categories={categories} />
+        <Allbooks />
+      </div>
+    </div>
+  );
 };
 
 export default Bookshelf;
