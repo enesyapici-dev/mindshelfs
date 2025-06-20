9f02e76649ce424bdf9801b2e95f9d914e5533d8
Enes YAPICI <enesyapici.dev@gmail.com>
2025-06-20 17:27:58 +0300

    feat(frontend): add book database methods to backend.js service

--- frontend/src/services/backend.js
+++ frontend/src/services/backend.js
@@ -1,3 +1,4 @@
+//movies
 export const getWatchedMovies = async () => {
   const response = await fetch("http://localhost:5555/movies");
 
@@ -27,6 +28,44 @@ export const updateMovieInDB = async (updatedMovie) => {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(updatedMovie),
+    }
+  );
+  const data = await response.json();
+  return data;
+};
+
+//books
+export const getBooks = async () => {
+  const response = await fetch("http://localhost:5555/books");
+  const data = await response.json();
+  return data.data;
+};
+
+export const addBookToDB = async (book) => {
+  const response = await fetch("http://localhost:5555/books", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(book),
+  });
+  const data = await response.json();
+  return data;
+};
+
+export const deleteBookFromDB = async (id) => {
+  const response = await fetch(`http://localhost:5555/books/${id}`, {
+    method: "DELETE",
+  });
+  const data = await response.json();
+  return data;
+};
+
+export const updateBookInDB = async (updatedBook) => {
+  const response = await fetch(
+    `http://localhost:5555/books/${updatedBook._id}`,
+    {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(updatedBook),
     }
   );
   const data = await response.json();
